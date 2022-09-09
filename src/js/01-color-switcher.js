0873bd8d6dabedaf6f9907d808649dfb4e8591f0
Oleksandr-Nehoda <iamsoul@ukr.net>
2022-09-10 02:09:23 +0300

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
+  const refs = {
+    btnStart: document.querySelector('[data-start]'),
+    btnStop: document.querySelector('[data-stop]'),
+    body: document.querySelector('body'),
+  }
+
+refs.btnStart.addEventListener('click', onChangeColor);
+refs.btnStop.addEventListener('click', onStopChangeColor);
+
+let timerId = null;
+
+function onChangeColor (evt) {
+
+     timerId = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+   
+  evt.target.setAttribute('disabled', 'true');
+  refs.btnStop.removeAttribute('disabled');
+}
+
+function onStopChangeColor (evt) {
+   clearInterval(timerId);
+   evt.target.setAttribute('disabled', 'true');
+   refs.btnStart.removeAttribute('disabled')
+
+}
